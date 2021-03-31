ad642bb174f8386fae005d4eecb0a28c8495ed55
OM SHIVA <OM SHIVA@LAPTOP-MNEHNPM7>
2021-03-31 23:12:04 +0530

    my first commit

---
+++ SportsShoppe/src/test/java/com/cp/sports/Service/IUserServiceTest.java
@@ -0,0 +1,27 @@
+package com.cp.sports.Service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.cp.sports.Entity.User;
+
+public class IUserServiceTest {
+	@Autowired
+	IUserService userService;
+	User user1=new User("CAPG123","123password","customer");
+	User user2=new User("CAPG125","125password","customer");
+
+	@Test
+	void addUser() {
+		
+		assertEquals("customer",userService.addUser(user1).getRole().toString());
+	}
+	
+	
+}
+
+
